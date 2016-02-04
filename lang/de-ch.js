31125bd4d521054347bf28e8f715236f16c2b117
Wiktor Walc <w.walc@cksource.com>
2016-02-04 11:58:54 +0100

    Updated language files. Added German (Switzerland) localization.

--- lang/de-ch.js
+++ lang/de-ch.js
@@ -2,6 +2,6 @@
 Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'blockquote', 'mk', {
-	toolbar: 'Одвоен цитат'
+CKEDITOR.plugins.setLang( 'blockquote', 'de-ch', {
+	toolbar: 'Zitatblock'
 } );
